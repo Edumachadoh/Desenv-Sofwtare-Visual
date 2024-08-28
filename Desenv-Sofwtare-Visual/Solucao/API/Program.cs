1ae5a4881c76b4f908b5e947862b3761b82034c4
Eduardo Henrique Machado <dudu.henrique.em@gmail.com>
2024-08-28 11:27:25 -0300

    Criando 3 endpoints

--- Desenv-Sofwtare-Visual/Solucao/API/Program.cs
+++ Desenv-Sofwtare-Visual/Solucao/API/Program.cs
@@ -1,6 +1,32 @@
+// Testar as APIs
+// - rest Client - Extensão do VS Code
+// - Postman
+// - Insomnia
+
+// MINIMAL APIs
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+// EndPoints - Funcionalidades
+// Configurar a URL e o método/verbo
+app.MapGet("/", () => "1 Hello World em C#!");
+
+app.MapGet("/segundaFunc", () => "2 Hello World em C#!");
+
+app.MapGet("/retornarendereco", () => {
+    dynamic endereco = new {
+        rua = "Parigot de Souzaw",
+        numero = 3000,
+        cep = "81290-350"
+    };
+
+    return endereco;
+});
+
+// criar Endpoint para receber informações
+// - Receber informações pela URL da requisição
+// - Pelo informações pelo corpo da requisição
+//Guardar as requisições em uma lista;
 
 app.Run();
